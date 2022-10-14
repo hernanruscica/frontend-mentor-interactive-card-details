9c78bd13b8b158a4096b6d347e26fbb37cdda41e
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-10-14 17:38:19 -0300

    Add files via upload
    form validation almost done !

--- scripts.js
+++ scripts.js
@@ -3,12 +3,15 @@ console.log("from scripts.js");
 const $d = document;
 const $cardholderNameInput = $d.getElementById('name');
 const $cardholderNameLabel = $d.getElementById('nameLabel');
+const $cardHolderNameError = $d.getElementById("name-error");
 
 const $cardNumberInput = $d.getElementById("number");
 const $cardNumberLabel = $d.getElementById("numberLabel");
+const $cardNumberError = $d.getElementById("number-error")
 
 const $cardDateMonthInput = $d.getElementById("exp-date-month-input");
 const $cardDateMonthLabel = $d.getElementById("dateMonthLabel");
+const $cardDateError = $d.getElementById("date-error")
 
 const $cardDateYearInput = $d.getElementById("exp-date-year-input");
 const $cardDateYearLabel = $d.getElementById("dateYearLabel");
@@ -16,9 +19,11 @@ const $cardDateYearLabel = $d.getElementById("dateYearLabel");
 const $cardCvcInput = $d.getElementById("cvc-input");
 const $cardCvcLabel = $d.getElementById("cvcLabel");
 
+const $btnConfirm = $d.getElementById("btn-confirm");
+
 let cardholderNameValue, cardNumberValue, dateMonthValue, dateYearValue, cardCvcValue;
 
-
+ 
 $d.addEventListener('keyup', (e) => {
 
     let id = e.target.id;
@@ -45,13 +50,39 @@ $d.addEventListener('keyup', (e) => {
     }    
 })
 
-const validateNumber = (value) => {
 
-    if (isNaN(value)){
-        return false;
-    }else{
-        return true;
+$btnConfirm.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    cardName = $cardholderNameInput.value;
+    cardNumber = $cardNumberInput.value;
+    cardMonth = $cardDateMonthInput.value;
+    cardYear = $cardDateYearInput.value;
+    cardCVC = $cardCvcInput.value;
+
+    /*Card Name validation and error messaging  */
+    if (cardName == ""){
+        $cardHolderNameError.classList.add("show");
+    }else {
+        $cardHolderNameError.classList.remove("show");
     }
-}
 
-console.log(validateNumber("1252"));
+    /*Card number validation and error messaging  */
+    if (cardNumber == "" || !/^[0-9]{16}$/.test(cardNumber)){
+        $cardNumberError.classList.add("show");
+    }else {
+        $cardNumberError.classList.remove("show");
+    }
+    
+    /*Month and Year date validation and error messaging  */
+    if (cardMonth == "" || cardYear == "" || !/^[0-9]{2}$/.test(cardMonth) ||  !/^[0-9]{2}$/.test(cardYear) ){
+        $cardDateError.classList.add("show");
+    }else{
+        $cardDateError.classList.remove("show");
+    }
+
+    /*CVC validation and error messaging  */
+
+} )
+
+/*console.log(!/^[0-9]{0,16}$/.test("34"));*/
